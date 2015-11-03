4bcf5df70c718845f39ac5765cd1425b36edd525
Li Lin <guru.lin@gmail.com>
2015-11-03 08:13:06 +0800

    now working after adding logic to reorder filters as suggested by SO thread. http://stackoverflow.com/questions/33458696/spring-boot-1-3-m5-oauth2-sso-does-not-work-with-spring-session

--- web-app/src/main/java/com/sloppycoder/webapp/MainAppApplication.java
+++ web-app/src/main/java/com/sloppycoder/webapp/MainAppApplication.java
@@ -1,22 +1,29 @@
 package com.sloppycoder.webapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.session.web.http.SessionRepositoryFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.filter.RequestContextFilter;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import javax.servlet.Filter;
 import java.security.Principal;
 import java.util.Map;
 
@@ -49,6 +56,39 @@ public class MainAppApplication extends WebMvcConfigurerAdapter {
     @RequestMapping("/")
     public String index() {
         return "redirect:dashboard";
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(RequestContextFilter.class)
+    public RequestContextFilter requestContextFilter() {
+
+        return new RequestContextFilter();
+    }
+
+    @Bean
+    public FilterRegistrationBean requestContextFilterChainRegistration(
+            @Qualifier("requestContextFilter") Filter securityFilter) {
+
+        FilterRegistrationBean registration = new FilterRegistrationBean(securityFilter);
+        registration.setName("requestContextFilter");
+
+        // note : must previous order of oAuth2ClientContextFilter
+        registration.setOrder(SessionRepositoryFilter.DEFAULT_ORDER + 1);
+
+        return registration;
+    }
+
+    @Bean
+    public FilterRegistrationBean sessionRepositoryFilterRegistration(
+            SessionRepositoryFilter sessionRepositoryFilter) {
+
+        FilterRegistrationBean registration = new FilterRegistrationBean(sessionRepositoryFilter);
+        registration.setName("springSessionRepositoryFilter");
+
+        // note : must following order of oAuth2ClientContextFilter
+        registration.setOrder(Integer.MAX_VALUE - 1);
+
+        return registration;
     }
 
     public static void main(String[] args) {
